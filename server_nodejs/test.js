138428d2b2e55cd846afb45f3b24ec74140a2232
alanTingTing <alan.yhting@gmail.com>
2019-06-29 15:29:05 +0800

    loginTest

--- server_nodejs/test.js
+++ server_nodejs/test.js
@@ -1,30 +1,45 @@
-var express=require('express');
-var app=express();
-app.get('/getUser',function(req,res){
-    var sql=require('mysql');
- 
-    //config for your database
-    var config={
-        user:'localhost',
-        password:'',
-        server:'127.0.0.1',   //這邊要注意一下!!
-        database:'hitchhike'
-    };
- 
-    //connect to your database
-    sql.connect(config,function (err) {
-        if(err) console.log(err);
- 
-        //create Request object
-        var request=new sql.Request();
-        request.query('select * from user',function(err,recordset){
-            if(err) console.log(err);
-            console.log("OK");
-            //send records as a response
-            res.send(recordset);
-        });
-    });
-});
-var server=app.listen(8080,function(){
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+//config for your database
+//server port :8080   local port :3000
+app.listen(8080, function () {
     console.log('Server is running!');
+});
+
+const db = mysql.createConnection({
+    user: 'root',
+    password: '',
+    server: '127.0.0.1',
+    database: 'test'
+});
+
+ //connect to your database
+db.connect((err) => {
+    if(err){
+        console.log(err);
+    }
+    console.log("Mysql Connected...");
+});
+
+app.get('/login/:name/:pwd', function (req, res) {
+    var name = req.params.name;
+    var pwd = req.params.pwd;
+    console.log(name);
+    console.log(pwd);
+    db.query(`select * from user where name = '${name}' AND pwd = '${pwd}' `, function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        if(result.length > 0){
+            console.log(result);
+            //send records as a response
+            res.status(200);
+            res.send("success");
+        }
+        else{
+            res.status(401);
+            res.send("fail");
+        }
+    });
 });
