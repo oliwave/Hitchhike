429d62083866933f0c1f59acefd1cc61a764a638
TristaRin <s105213018@mail1.ncnu.edu.tw>
2019-07-07 14:51:19 +0800

    jwt done

--- server_nodejs/signInUp/jwt.js
+++ server_nodejs/signInUp/jwt.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
+var db = require('../db.js');
 
-function getToken(){
-
-    const token = jwt.sign(/*user uid*/ , 'thisismynewcourse');
+function getToken(uid) {
+    const token = jwt.sign(uid, 'thisismynewcourse');
     return token;
 }
-module.exports = getToken();
+module.exports = getToken;
--- server_nodejs/signInUp/login.js
+++ server_nodejs/signInUp/login.js
@@ -1,23 +1,22 @@
 var db = require('../db.js');
 var express = require('express');
-//var getToken = require('./jwt.js');
+var getToken = require('./jwt.js');
 const router = new express.Router()
 
 router.get('/login/:uid/:pwd', function (req, res) {
     var uid = req.params.uid;
     var pwd = req.params.pwd;
-    //var token = getToken;
+    var token = getToken(uid);
     const sql = `select * from user where uid = '${uid}' AND pwd = '${pwd}' `
     db.query(sql, function (err, result) {
         if (err) {
             console.log(err);
         }
         if(result.length > 0){
-            console.log(result);
             //send records as a response
             res.status(200);
-            res.send("success");
-            //res.send(token);
+            // res.send("success");
+            res.send(token);
         }
         else{
             res.status(401);
