182d30ff5995c90d3768d87388f05a7bcf5bcb82
OliverWangWei <a0924100192@gmail.com>
2019-07-16 16:14:02 +0800

    Modifying LoginPage to const constructor (testing)

--- client_flutter/lib/src/screen/login_page.dart
+++ client_flutter/lib/src/screen/login_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   static const String routeName = '/login_page';
 
+  const LoginPage();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
