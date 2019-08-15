bbc6ee1a1e41b7b4df5cebf075b323c7ae8bc089
OliverWangWei <a0924100192@gmail.com>
2019-08-15 21:52:23 +0800

    Feature : send fcm token to server when the app is first launched.

--- client_flutter/lib/init_setting.dart
+++ client_flutter/lib/init_setting.dart
@@ -1,25 +1,44 @@
 import 'package:permission_handler/permission_handler.dart';
 
+import './src/resources/repository.dart';
+import './src/resources/restful/request_method.dart' show FcmTokenRequest;
+
 /// [init] is a comile-time constant.
 /// [https://dart.dev/guides/language/language-tour#final-and-const]
-const init = InitSetting();
+final init = InitSetting();
 
 class InitSetting {
-  const InitSetting();
+  static final _prefs = Repository.getSimpleStorage;
+  static final _api = Repository.getApi;
+  static final _fcm = Repository.getCloudMessageHandler;
 
-  Future runInitSetting() {
-    return Future.wait(
-      [
-        _setPermission(),
-      ],
-    );
+  Future<void> runInitSetting() async {
+    await _setLocationPermission();
+    await _prefs.init();
+    await _fcm.init();
+    _checkLaunchingTime();
   }
 
-  Future _setPermission() async {
+  Future<void> _setLocationPermission() async {
     final permissions = await PermissionHandler()
         .requestPermissions([PermissionGroup.locationWhenInUse]);
 
     print('show the location permission status : '
         '${permissions[PermissionGroup.locationWhenInUse]}');
+  }
+
+  void _checkLaunchingTime() {
+    final hasMultipleLaunched =
+        _prefs.getBool(TargetSourceString.hasMultipleLaunched);
+
+    if (!hasMultipleLaunched) {
+      final response = _api.sendHttpRequest(FcmTokenRequest(
+        fcmToken: _fcm.fcmToken,
+        jwtToken: '',
+      ));
+
+      _prefs.setBool(TargetSourceString.hasMultipleLaunched, true).then(
+          (onValue) => print('hasMultipleLaunched is set with : $onValue'));
+    }
   }
 }
