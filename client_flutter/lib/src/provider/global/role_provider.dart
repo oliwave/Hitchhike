efeec12b1b8f71bdd20d7c095c91fb70c34720fe
OliverWangWei <a0924100192@gmail.com>
2019-08-27 14:52:02 +0800

    Feature : record list of routeLatitude and routeLongitude for showing route on map.

--- client_flutter/lib/src/provider/global/role_provider.dart
+++ client_flutter/lib/src/provider/global/role_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../resources/repository.dart';
+import '../../resources/source_collection.dart';
 
 /// Recording the current client information
 ///
@@ -8,13 +9,37 @@ import '../../resources/repository.dart';
 /// if the client is in the matched mode, the [isDriver] can determine what role the
 /// client is.
 class RoleProvider with ChangeNotifier {
-  static final _prefs = Repository.getSimpleStorage;
+  RoleProvider._();
 
-  bool _isMatched = _prefs.getBool(TargetSourceString.isMatched);
-  String _role = _prefs.getString(TargetSourceString.role);
+  factory RoleProvider() {
+    return _roleProvider;
+  }
+
+  static final _roleProvider = RoleProvider._();
+
+  SimpleStorage _prefs;
+  bool _isMatched;
+  String _role;
+  List<String> _routeLatitude;
+  List<String> _routeLongitude;
+
+  bool _isFirst = true;
 
   bool get isMatched => _isMatched;
   String get role => _role;
+
+  /// This method should only be triggered one time.
+  void init() {
+    if (_isFirst) {
+      _prefs = Repository.getSimpleStorage;
+      _isMatched = _prefs.getBool(TargetSourceString.isMatched);
+      _role = _prefs.getString(TargetSourceString.role);
+      _routeLatitude = _prefs.getStringList(TargetSourceString.routeLatitude);
+      _routeLongitude = _prefs.getStringList(TargetSourceString.routeLongitude);
+      _isFirst = false;
+      return;
+    }
+  }
 
   set isMatched(bool isMatched) {
     _isMatched = isMatched;
@@ -30,6 +55,13 @@ class RoleProvider with ChangeNotifier {
     if (_isMatched) {
       _prefs.setString(TargetSourceString.role, _role);
       _prefs.setBool(TargetSourceString.isMatched, _isMatched);
+      _prefs.setStringList(TargetSourceString.routeLatitude, _routeLatitude);
+      _prefs.setStringList(TargetSourceString.routeLongitude, _routeLongitude);
+    } else {
+      _prefs.setString(TargetSourceString.role, null);
+      _prefs.setBool(TargetSourceString.isMatched, false);
+      _prefs.setStringList(TargetSourceString.routeLatitude, null);
+      _prefs.setStringList(TargetSourceString.routeLongitude, null);
     }
     super.dispose();
   }
