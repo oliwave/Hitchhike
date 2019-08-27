c9b2a9a8e441142e8346ee3b65fece470f5ac5c2
OliverWangWei <a0924100192@gmail.com>
2019-08-27 15:37:15 +0800

    Refactor : change the spec of creating the size of widget.

--- client_flutter/lib/src/util/platform_info.dart
+++ client_flutter/lib/src/util/platform_info.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
 class PlatformInfo {
-
   PlatformInfo._();
 
-  static const double _baseHeight = 650.0;
+  // static const double _baseHeight = 650.0;
 
   static BuildContext context;
 
   static double screenAwareSize(double size) {
     // final height = size * MediaQuery.of(context).size.height / _baseHeight;
-    final height = size * 1.19;
+    final height = size * (MediaQuery.of(context).size.height > 650 ? 1.2 : 1.1);
     // print(height);
     return height;
   }
