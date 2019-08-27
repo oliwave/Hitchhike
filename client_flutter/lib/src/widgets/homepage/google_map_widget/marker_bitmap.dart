80ced775e9775cf84b0e04652a6dc9ac7dd1032e
OliverWangWei <a0924100192@gmail.com>
2019-08-27 15:21:27 +0800

    Refactor : use async/await to instantiate bitmap resource instead of then.

--- client_flutter/lib/src/widgets/homepage/google_map_widget/marker_bitmap.dart
+++ client_flutter/lib/src/widgets/homepage/google_map_widget/marker_bitmap.dart
@@ -1,23 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MarkerBitmap {
-  MarkerBitmap._(context) {
-    _context = context;
-    
-    _initializeBitmap();
-  }
+  MarkerBitmap._();
 
-  factory MarkerBitmap([BuildContext context]) {
-    if (_markerBitmap == null) {
-      return _markerBitmap = MarkerBitmap._(context);
-    } else {
-      return _markerBitmap;
-    }
-  }
+  factory MarkerBitmap() => _markerBitmap;
 
-  static MarkerBitmap _markerBitmap;
+  static final MarkerBitmap _markerBitmap = MarkerBitmap._();
   BuildContext _context;
 
   static const Map<String, String> _raw = {
@@ -27,18 +17,24 @@ class MarkerBitmap {
   };
 
   Map<String, BitmapDescriptor> bitmaps = {};
-  
+
   static const String motor = 'motor';
   static const String car = 'car';
   static const String compass = 'compass';
 
-  void _initializeBitmap() {
-    _raw.forEach((text, path) {
-      _createBitmap(path: path).then((bitmap) {
-        print('Get the $text bitmap!');
-        bitmaps[text] = bitmap;
-      });
-    });
+  Future<void> initializeBitmap(BuildContext context) async {
+    if (bitmaps.isNotEmpty) return;
+    _context = context;
+    print('Initialize the MarkerBitmap!');
+
+    final text = _raw.keys.toList();
+    final path = _raw.values.toList();
+
+    for (int i = 0; i < text.length; i++) {
+      final target = text[i];
+      bitmaps[target] = await _createBitmap(path: path[i]);
+      print('Get the $text bitmap!');
+    }
   }
 
   Future<BitmapDescriptor> _createBitmap({@required String path}) {
