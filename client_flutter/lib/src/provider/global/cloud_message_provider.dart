3ab76d345edf6ce9295de15389619e2ba3df2ad1
OliverWangWei <a0924100192@gmail.com>
2019-08-27 14:29:49 +0800

    Create cloud message provider to provide global state management.

--- client_flutter/lib/src/provider/global/cloud_message_provider.dart
+++ client_flutter/lib/src/provider/global/cloud_message_provider.dart
@@ -1,5 +1,50 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+import '../../resources/repository.dart';
+import '../../logics/fcm/fcm_action_manager.dart';
+
 class CloudMessageProvider with ChangeNotifier {
-  
+  CloudMessageProvider._() {
+    _managerInit();
+    _subscribeFcmEvent();
+  }
+
+  factory CloudMessageProvider() {
+    return _cloudMessageProvider;
+  }
+
+  static final _cloudMessageProvider = CloudMessageProvider._();
+  static final _fcm = Repository.getCloudMessageHandler;
+
+  StreamSubscription _fcmEventSubscription;
+  BuildContext _context;
+  FcmActionManager _fcmActionManager;
+
+  set context(BuildContext context) => _context = context;
+
+  void _subscribeFcmEvent() {
+    _fcmEventSubscription = _fcm.fcmEvent.listen(
+      (Map<String, dynamic> message) {
+        print('Emit an fcm event!');
+        if (_context != null)
+          _fcmActionManager.messageConsumer(message, _context);
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _fcmEventSubscription.cancel();
+    super.dispose();
+  }
+
+  void _managerInit() {
+    _fcmActionManager = FcmActionManager(_registerNotifyListeners);
+  }
+
+  void _registerNotifyListeners() {
+    notifyListeners();
+  }
 }
