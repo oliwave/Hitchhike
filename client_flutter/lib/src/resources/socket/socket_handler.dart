c9242c192665a7d00a19c02b6311c6bdc92e0911
OliverWangWei <a0924100192@gmail.com>
2019-09-03 20:40:16 +0800

    Define basic socket io event in socket handler.

--- client_flutter/lib/src/resources/socket/socket_handler.dart
+++ client_flutter/lib/src/resources/socket/socket_handler.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+
+import 'package:flutter/Material.dart';
+
 import 'package:adhara_socket_io/adhara_socket_io.dart';
+
+import '../../resources/repository.dart';
 
 class SocketHandler {
   SocketHandler._();
@@ -13,13 +19,26 @@ class SocketHandler {
   SocketIO _socket;
   bool _isConnected;
 
+  final _driverPositionController = StreamController<Map<String, String>>();
+  final _revokeDriverPositionController = StreamController<String>();
+  final _chatController = StreamController<Map<String, dynamic>>();
+
   bool get isConnected => _isConnected;
+
+  /// {'lat': XXX, 'lng': XXX, 'heading': XXX}
+  Stream<Map<String, String>> get getDriverPositionStream =>
+      _driverPositionController.stream;
+  Stream<String> get getRevokeDriverPositionStream =>
+      _revokeDriverPositionController.stream;
+  Stream<Map<String, dynamic>> get getChatStream => _chatController.stream;
 
   Future<SocketIO> connectSocketServer() async {
     if (!_isConnected) {
       _socket = await _manager.createInstance('https://socket.hitchhike.ml');
       _socket.connect();
       _isConnected = true;
+      _subscribeEvents();
+
       return _socket;
     } else {
       return null;
@@ -31,5 +50,43 @@ class SocketHandler {
       _isConnected = false;
       await _manager.clearInstance(_socket);
     }
+  }
+
+  void emitEvent({
+    @required String eventName,
+    @required dynamic content,
+  }) {
+    _socket.emit(eventName, content);
+  }
+
+  _subscribeEvents() {
+    // The event that driver needs to listen.
+    _addDataToSink(
+      eventName: SocketEventName.revokeDriverPosition,
+      sink: _revokeDriverPositionController.sink,
+    );
+    // The event that passenger needs to listen.
+    _addDataToSink(
+      eventName: SocketEventName.driverPosition,
+      sink: _driverPositionController.sink,
+    );
+
+    _addDataToSink(
+      eventName: SocketEventName.chat,
+      sink: _chatController.sink,
+    );
+  }
+
+  _addDataToSink({
+    @required String eventName,
+    @required StreamSink sink,
+  }) {
+    _socket.on(eventName, (data) => sink.add(data));
+  }
+
+  dispose() {
+    _driverPositionController.close();
+    _revokeDriverPositionController.close();
+    _chatController.close();
   }
 }
