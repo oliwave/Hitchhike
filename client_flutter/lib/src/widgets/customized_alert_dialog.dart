aa5d97d078ae776d038c28d335f1d921a94d2815
OliverWangWei <a0924100192@gmail.com>
2019-09-15 20:48:01 +0800

    Refactor : extract the fcmAlertDialog method to a widget class.

---
+++ client_flutter/lib/src/widgets/customized_alert_dialog.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+Future<dynamic> customizedAlertDialog({
+  @required BuildContext context,
+  @required Widget title,
+  @required Widget content,
+  @required String confirmButtonName,
+  @required VoidCallback confirmCallback,
+  String cancelButtonName,
+  VoidCallback cancelCallback,
+  bool barrierDismissible = true,
+}) {
+  if (cancelButtonName != null && cancelCallback != null) {
+    return showDialog(
+      context: context,
+      barrierDismissible: barrierDismissible,
+      builder: (_) => AlertDialog(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        actions: <Widget>[
+          if (cancelButtonName != null)
+            FlatButton(
+              child: Text(
+                cancelButtonName,
+                style: const TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              onPressed: cancelCallback,
+            ),
+          FlatButton(
+            child: Text(confirmButtonName),
+            onPressed: confirmCallback,
+          )
+        ],
+        title: title,
+        content: content,
+      ),
+    );
+  } else {
+    throw Exception(
+        'cancelButtonName and cancelCallback must be specified together or not!');
+  }
+}
