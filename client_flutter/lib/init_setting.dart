ee0d5ee6513890af8acbbf897caa69486fe83ab0
OliverWangWei <a0924100192@gmail.com>
2019-09-15 14:04:46 +0800

    Refactor : extract the logics of managing fcm token to FcmTokenManager.

--- client_flutter/lib/init_setting.dart
+++ client_flutter/lib/init_setting.dart
@@ -1,24 +1,21 @@
 import 'dart:async';
 
+import 'package:client_flutter/src/provider/provider_collection.dart';
 import 'package:flutter/material.dart';
 
 import 'package:permission_handler/permission_handler.dart';
-import 'package:connectivity/connectivity.dart';
 
 import './src/resources/file/directory_access.dart';
 import './src/widgets/homepage/google_map_widget/marker_bitmap.dart';
 import './src/resources/repository.dart';
-import './src/resources/restful/request_method.dart' show FcmTokenRequest;
 import './src/provider/provider_collection.dart'
     show LocationProvider, FavoriteRoutesProvider, ConnectivityProvider;
 
-/// TODO : Must add Wifi and data permission to make sure every user behavior is correct.
 final init = InitSetting();
 
 class InitSetting {
   static final _prefs = Repository.getSimpleStorage;
-  static final _api = Repository.getApi;
-  static final _fcm = Repository.getCloudMessageHandler;
+  static final _fcmHandler = Repository.getCloudMessageHandler;
   static final _db = Repository.getDatabaseHandler;
 
   static final _bitmap = MarkerBitmap();
@@ -28,69 +25,24 @@ class InitSetting {
 
   Future<void> runInitSetting(BuildContext context) async {
     await _setLocationPermission();
-    _networkConnection();
     await _prefs.init();
-    await _fcm.init();
+
+    _connectivityProvider.networkConnection();
+    
     await _bitmap.initializeBitmap(context);
     await DirectoryAccess.initDirectory();
     await _locationProvider.initializePosition();
     await _db.init();
     await _favoriteRoutesProvider.initRoutesList();
-
-    _checkHasFcmToken();
   }
 
   Future<void> _setLocationPermission() async {
-    _fcm.iOSPermissionRequest();
+    _fcmHandler.iOSPermissionRequest();
 
     final permissions = await PermissionHandler()
-        .requestPermissions([PermissionGroup.locationWhenInUse]);
+        .requestPermissions([PermissionGroup.locationAlways]);
 
     print('show the location permission status : '
-        '${permissions[PermissionGroup.locationWhenInUse]}');
-  }
-
-  Future<void> _checkHasFcmToken() async {
-    final hasFcmToken = _prefs.getBool(TargetSourceString.hasFcmToken);
-    print(
-        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhas'
-        'FcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
-        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
-        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
-        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
-        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
-        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken is $hasFcmToken');
-
-    /// TODO : refactor to use hasSendFcmToken instead of hasFcmToken
-    if (!hasFcmToken) {
-      final response = await _api.sendHttpRequest(
-        FcmTokenRequest(
-          fcmToken: _fcm.fcmToken,
-          jwtToken: '',
-        ),
-      );
-
-      print(
-          'fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response '
-          'fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response '
-          'fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response '
-          'fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response '
-          'fcm response : ${response.statusCode}');
-
-      final result = await _prefs.setBool(
-        TargetSourceString.hasFcmToken,
-        response.statusCode == 200,
-      );
-
-      print('hasFcmToken is set with : $result');
-    }
-  }
-
-  void _networkConnection() {
-    Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
-      print(
-          'Connectivity has been triggered and the network status is $result');
-      _connectivityProvider.networkStatus = result != ConnectivityResult.none;
-    });
+        '${permissions[PermissionGroup.locationAlways]}');
   }
 }
---
+++ client_flutter/lib/src/logics/fcm/fcm_token_manager.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import '../notify_manager.dart';
+import '../../resources/repository.dart';
+import '../../resources/restful/request_method.dart' show FcmTokenRequest;
+
+class FcmTokenManager extends NotifyManager {
+  FcmTokenManager(VoidCallback notifyListeners) : super(notifyListeners);
+
+  final _prefs = Repository.getSimpleStorage;
+  final _api = Repository.getApi;
+  final _fcm = Repository.getCloudMessageHandler;
+
+  bool _hasSentFcmToken = false;
+
+  String _fcmToken;
+
+  bool get hasSentFcmToken => _hasSentFcmToken;
+
+  Future<void> initSendFcmToken() async {
+    // Get the boolean of hasSentFcmToken.
+    _hasSentFcmToken = _prefs.getBool(TargetSourceString.hasSentFcmToken);
+
+    // Didn't send fcm token before.
+    if (!_hasSentFcmToken) {
+      // Fetch Fcm token from Google api or local storage.
+      _fcmToken = await _fcm.firebaseMessaging.getToken();
+
+      print('fcmToken is $_fcmToken');
+
+      _sendFcmToken();
+    } else {
+      return print('hasSentFcmToken has been initialized!');
+    }
+  }
+
+  Future<void> _sendFcmToken() async {
+    print(
+        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhas'
+        'FcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
+        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
+        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
+        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
+        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken'
+        'hasFcmTokenhasFcmTokenhasFcmTokenhasFcmToken is $hasSentFcmToken');
+
+    // Send fcm token to the server if hasSentFcmToken is false.
+    final response = await _api.sendHttpRequest(
+      FcmTokenRequest(
+        fcmToken: _fcmToken,
+        jwtToken: '',
+      ),
+    );
+
+    print(
+        'fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response '
+        'fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response '
+        'fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response '
+        'fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response fcm response '
+        'fcm response : ${response.statusCode}');
+
+    // Record the runtime _hasSentFcmToken.
+    _hasSentFcmToken = response.statusCode == 200;
+
+    /// Write the data to simple storage.
+    await _prefs.setBool(
+      TargetSourceString.hasSentFcmToken,
+      hasSentFcmToken,
+    );
+
+    print('hasFcmToken is set with $hasSentFcmToken');
+  }
+}
