85e4d54b9e5112086ca501ca1932b51fbe589a2f
TristaRin <s105213018@mail1.ncnu.edu.tw>
2019-09-18 15:22:19 +0800

    new userInfo

---
+++ server_nodejs/profile/userInfo.js
@@ -0,0 +1,39 @@
+var db = require('../db.js');
+var express = require('express');
+const auth = require('../auth.js');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+const router = new express.Router();
+
+router.post('/userInfo', auth.auth, function (req, res) {
+    var uid = auth.uid;
+    const sql = `SELECT * from user where uid=${uid}`
+    db.query(sql, function (err, result) {
+        if (err) {
+            res.send({ "status": "fail" });
+            console.log(err);
+        }
+        else {
+            res.send({
+                "uid": result[0].uid,
+                "pwd": result[0].pwd,
+                "name": result[0].name,
+                "photo": result[0].photo,
+                "gender": result[0].gender,
+                "department": result[0].department,
+                "birthday": result[0].birthday,
+                "star": result[0].star,
+                "score": result[0].score,
+                "d_rank": result[0].d_rank,
+                "p_rank": result[0].p_rank,
+                "car_num": result[0].car_num,
+                "d_times": result[0].d_times,
+                "p_times": result[0].p_times,
+                "token": result[0].token
+            })
+        }
+    });
+});
+module.exports = router;
--- server_nodejs/server.js
+++ server_nodejs/server.js
@@ -3,12 +3,14 @@ var login = require('./signInUp/login.js');
 var signUp = require('./signInUp/signUp.js');
 var profile = require('./profile/profile.js');
 var pair = require('./pair/pair.js');
+var userInfo = require('./profile/userInfo.js');
 var app = express();
 
 app.use(login);
 app.use(signUp);
 app.use(profile);
 app.use(pair);
+app.use(userInfo);
 
 //server port :8080   local port :3000
 app.listen(3000, function () {
