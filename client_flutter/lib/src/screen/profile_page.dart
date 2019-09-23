d1759bc44bf60b5da41eb1d468fae8b3d50e7f28
s105213015 <s105213015@mail1.ncnu.edu.tw>
2019-09-23 20:10:44 +0800

    Feature : create sign up page

--- client_flutter/lib/src/screen/profile_page.dart
+++ client_flutter/lib/src/screen/profile_page.dart
@@ -1,14 +1,179 @@
+// get jwt
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   static const String routeName = '/profile_page';
+
+  @override
+  State<StatefulWidget> createState() => ProfilePageState();
+}
+
+class ProfilePageState extends State<ProfilePage> {
+  final formKey = GlobalKey<FormState>(); // GlobalKey: to access form
+
+  String name = '';
+  String gender = '';
+  String birthday = '';
+  String star = '';
+  String email = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ProfilePage'),
+        backgroundColor: Colors.teal[400],
+        title: Text('編輯個人檔案'),
+        actions: <Widget>[
+          FlatButton(
+            textColor: Colors.white,
+            onPressed: () {},
+            child: Text("完成"),
+            shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
+          ),
+        ],
       ),
+      body: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(20.0),
+          child: ListView(
+            children: <Widget>[
+              Form(
+                key: formKey,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: 120.0,
+                      height: 120.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.teal[400],
+                        image: DecorationImage(
+                          image: AssetImage(''
+                              //Utils.getImgPath(''),
+                              ),
+                        ),
+                      ),
+                    ),
+                    FlatButton(
+                      child: Text('上傳大頭貼照'),
+                      onPressed: () {
+                        // 上傳照片
+                      },
+                    ),
+                    nameField(),
+                    genderField(),
+                    birthdayField(),
+                    starField(),
+                    emailField(),
+                    Container(margin: EdgeInsets.only(top: 25.0)),
+                    submitButton(),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ),
+    );
+  }
+
+  Widget nameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: '姓名',
+        hintText: '',
+      ),
+      onSaved: (String value) {
+        name = value;
+      },
+    );
+  }
+
+  Widget genderField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: '性別',
+        hintText: '',
+      ),
+      onSaved: (String value) {
+        gender = value;
+      },
+    );
+  }
+
+  var _chooseDate;
+  _showDataPicker() async {
+    var date = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1870),
+      lastDate: DateTime(2050),
+    );
+    setState(() {
+      this._chooseDate = date.toString().split(" ")[0];
+    });
+  }
+
+  var _time;
+  Widget birthdayField() {
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          keyboardType: TextInputType.datetime, // 鍵盤樣式
+          decoration: InputDecoration(
+            labelText: '生日',
+            hintText: '',
+          ),
+          onSaved: (String value) {
+            birthday = value;
+          },
+        ),
+        RaisedButton(
+          child: Text(_time == null ? '選擇日期' : _time),
+          onPressed: () => _showDataPicker(),
+        ),
+      ],
+    );
+  }
+
+  Widget starField() {
+    return TextFormField(
+      // textAlign: TextAlign.center,
+      decoration: InputDecoration(
+        labelText: '星座',
+        hintText: '',
+      ),
+      onSaved: (String value) {
+        star = value;
+      },
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress, // 鍵盤樣式
+      decoration: InputDecoration(
+        labelText: 'E-mail',
+        hintText: '',
+      ),
+      onSaved: (String value) {
+        email = value;
+      },
+      validator: (String value) {
+        return value.contains('@') ? 'Do not use the @ char.' : null;
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text('確認修改'),
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('$name,$email');
+        }
+      },
     );
   }
 }
---
+++ client_flutter/lib/src/screen/sign_up_profile_name_page.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'sign_up_profile_gender_page.dart';
+
+class NamePage extends StatefulWidget {
+  NamePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _NamePageState();
+  }
+}
+
+class _NamePageState extends State<NamePage> {
+  final _formKey = GlobalKey<FormState>(); // GlobalKey: to access form
+
+  TextEditingController controller;
+  TextEditingController controller2;
+
+  @override
+  void initState() {
+    controller = TextEditingController();
+    controller2 = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Map user = Map.of(ModalRoute.of(context).settings.arguments);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal[400],
+        title: Text(widget.title),
+      ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
+          // 點空白收起鍵盤
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Container(
+          color: Colors.white,
+          child: Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding:
+                        EdgeInsets.only(top: 50.0, right: 50.0, left: 50.0),
+                    child: Column(
+                      children: <Widget>[
+                        usernameField(user),
+                        SizedBox(height: 25.0),
+                        passwordField(user),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    child: Column(
+                      verticalDirection: VerticalDirection.up,
+                      children: <Widget>[
+                        Container(
+                          width: double.infinity,
+                          height: 55.0,
+                          child: FlatButton(
+                              disabledTextColor: Colors.teal[50], // 按鈕禁用時顏色
+                              color: Colors.teal,
+                              onPressed: () {
+                                if (!_formKey.currentState.validate()) {
+                                  return null;
+                                } else {
+                                  _formKey.currentState.save();
+                                  Navigator.push<String>(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            GenderPage(title: "Sign Up"),
+                                        settings: RouteSettings(
+                                          arguments: user,
+                                        ),
+                                      ));
+                                }
+                              },
+                              child: Text("Next")),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget usernameField(Map user) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+          icon: Icon(
+            Icons.assignment_ind, //contacts,
+            color: Colors.teal[400],
+            size: 24.0,
+          ),
+          hintText: 'Your name',
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+              width: 2.0,
+            ),
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(
+              Icons.highlight_off,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              controller.clear();
+            },
+          )),
+      inputFormatters: <TextInputFormatter>[
+        LengthLimitingTextInputFormatter(50) //限制長度
+      ],
+      validator: (value) => value.isEmpty ? 'Can not be empty.' : null,
+      onSaved: (String value) {
+        user['name'] = value;
+      },
+    );
+  }
+
+  Widget passwordField(Map user) {
+    return TextFormField(
+      controller: controller2,
+      obscureText: true, // 密碼輸入後顯示為點
+      decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock_outline,
+            color: Colors.teal[400],
+            size: 24.0,
+          ),
+          hintText: 'Password',
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+              width: 2.0,
+            ),
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.highlight_off),
+            color: Colors.grey,
+            onPressed: () {
+              controller2.clear();
+            },
+          )),
+      inputFormatters: <TextInputFormatter>[
+        LengthLimitingTextInputFormatter(50) //限制長度
+      ],
+      validator: (value) => value.isEmpty ? 'Can not be empty.' : null,
+      onSaved: (String value) {
+        user['password'] = value;
+      },
+    );
+  }
+}
