dddc57ff8baff0d38966df26f88c354e81dd6845
OliverWangWei <a0924100192@gmail.com>
2019-10-12 01:07:43 +0800

    Feature : complete the development of  chatting UI.

--- client_flutter/lib/src/widgets/chatting_page/chat_list_view.dart
+++ client_flutter/lib/src/widgets/chatting_page/chat_list_view.dart
@@ -4,6 +4,10 @@ import 'package:provider/provider.dart';
 import 'package:bubble/bubble.dart';
 
 import '../../provider/provider_collection.dart' show ChattingProvider;
+import '../../resources/repository.dart';
+import '../../widgets/loading_container.dart';
+import '../../model/chat_item.dart';
+
 import './chat_bubble_style.dart';
 
 class ChatListView extends StatelessWidget {
@@ -17,20 +21,40 @@ class ChatListView extends StatelessWidget {
       padding: EdgeInsets.only(bottom: 8.0),
       child: Consumer<ChattingProvider>(
         builder: (context, provider, child) {
-          return ListView.builder(
-            reverse: true,
-            itemCount: provider.data.length,
-            controller: provider.listController,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4.0),
-                child: Bubble(
-                  nip: BubbleNip.no,
-                  style: index % 2 == 0
-                      ? ChatBubbleStyle.me
-                      : ChatBubbleStyle.somebody,
-                  child: Text(provider.data[index]),
+          return FutureBuilder(
+            future: provider.chatRecordManager.messages,
+            builder: (
+              BuildContext context,
+              AsyncSnapshot<List<ChatItem>> snapshot,
+            ) {
+              if (!snapshot.hasData) {
+                return LoadingContainer();
+              }
+
+              final chatItems = snapshot.data;
+
+              return NotificationListener(
+                child: ListView.builder(
+                  reverse: true,
+                  // itemCount: provider.testData.length,
+                  itemCount: chatItems.length,
+                  controller: provider.listController,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      child: Bubble(
+                        nip: BubbleNip.no,
+                        // style: index % 2 == 0
+                        style: chatItems[index].character == Character.me
+                            ? ChatBubbleStyle.me
+                            : ChatBubbleStyle.somebody,
+                        // child: Text(provider.testData[index]),
+                        child: Text(chatItems[index].text),
+                      ),
+                    );
+                  },
                 ),
+                onNotification: provider.reachTheTopOfListLisnter(),
               );
             },
           );
