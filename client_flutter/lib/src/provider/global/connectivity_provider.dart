dfa4916eb5e416971f0c3fea8db82385961ffb61
OliverWangWei <a0924100192@gmail.com>
2019-10-12 00:52:49 +0800

    Enable clients connect to server through socket

--- client_flutter/lib/src/provider/global/connectivity_provider.dart
+++ client_flutter/lib/src/provider/global/connectivity_provider.dart
@@ -16,6 +16,7 @@ class ConnectivityProvider with ChangeNotifier {
   static final _connectivityProvider = ConnectivityProvider._();
   final _bulletin = BulletinProvider();
   final _fcmHandler = Repository.getCloudMessageHandler;
+  final _socketHandler = Repository.getSocketHandler;
   final _fcmProvider = CloudMessageProvider();
 
   bool networkStatus;
@@ -38,10 +39,15 @@ class ConnectivityProvider with ChangeNotifier {
           'Connectivity has been triggered and the network status is $result');
       _connectivityProvider.networkStatus = result != ConnectivityResult.none;
 
-      if (_connectivityProvider.networkStatus &&
-          !_fcmProvider.fcmTokenManager.hasSentFcmToken) {
-        _fcmHandler.messageConfiguration();
-        _fcmProvider.fcmTokenManager.initSendFcmToken();
+      if (_connectivityProvider.networkStatus) {
+        _socketHandler.connectSocketServer();
+
+        if (!_fcmProvider.fcmTokenManager.hasSentFcmToken) {
+          _fcmHandler.messageConfiguration();
+          _fcmProvider.fcmTokenManager.initSendFcmToken();
+        }
+      } else {
+        _socketHandler.disconnetSocket();
       }
     });
   }
