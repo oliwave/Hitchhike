752863a9b552455870db0ca12d7c894f61549d33
OliverWangWei <a0924100192@gmail.com>
2019-10-19 10:27:11 +0800

    Remove named constructor fromInstance.

--- client_flutter/lib/src/model/friend_item.dart
+++ client_flutter/lib/src/model/friend_item.dart
@@ -1,18 +1,30 @@
+import 'package:flutter/material.dart';
+
 class FriendItem {
+  FriendItem({
+    @required this.room,
+    @required this.name,
+    @required this.avatar,
+  }) : hasNewMessage = false;
+
   FriendItem.fromDb(Map<String, dynamic> parsedJson)
       : room = parsedJson['room'],
         name = parsedJson['name'],
-        avatar = parsedJson['avatar'];
+        avatar = parsedJson['avatar'],
+        hasNewMessage = parsedJson['hasNewMessage'] == 1;
 
   Map<String, dynamic> toMapForDb() {
     return <String, dynamic>{
       'room': room,
       'name': name,
       'avatar': avatar,
+      'hasNewMessage': hasNewMessage ? 1 : 0,
     };
   }
 
+  /// [room] stands for room id that corresponds to a unique friend.
   final String room;
   final String name;
   final String avatar;
+  final bool hasNewMessage;
 }
