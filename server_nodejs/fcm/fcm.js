d7682369e34e6955629103c83f02deb8c9926c0a
alanTingTing <alan.yhting@gmail.com>
2019-10-19 12:44:12 +0800

    fcm fixed

--- server_nodejs/fcm/fcm.js
+++ server_nodejs/fcm/fcm.js
@@ -9,13 +9,13 @@ admin.initializeApp({
 //let registrationToken = 'e3SZXZnimco:APA91bEuBrZ1AeihGh-nTWYxyYxjcy_Dbb9YHv7m_HxMPAwdHbeZTvAwUqB0lfjvGjsQc5zdqEPxCVnDUn04fsjxPogCt-bcfnAC0pYlLy-eMImxCzF6ouSNEIY6TqQ-IcLpLEZZw4Dm';
 
 // 對這個已註冊 token 的手機裝置傳送訊息
-admin.messaging().send(message)
-    .then((response) => {
-        // Response 是一個字串型別的 message ID.
-        console.log('Successfully sent message:', response);
-    })
-    .catch((error) => {
-        console.log('Error sending message:', error);
-    });
+// admin.messaging().send(message)
+//     .then((response) => {
+//         // Response 是一個字串型別的 message ID.
+//         console.log('Successfully sent message:', response);
+//     })
+//     .catch((error) => {
+//         console.log('Error sending message:', error);
+//     });
 
 module.exports = admin;
