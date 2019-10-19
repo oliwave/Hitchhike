a904688366333527a3122a9e9378c65ba21c2d62
alanTingTing <alan.yhting@gmail.com>
2019-10-19 20:53:12 +0800

    socket router

--- server_nodejs/chat/socket.js
+++ server_nodejs/chat/socket.js
@@ -4,71 +4,75 @@ const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const router = new express.Router();
 
 let onlineUserMap = {};
 // 當有新建立的 socket 連線
-io.on('connection', auth.auth, (socket) => {
-    var uid = auth.uid;
-    // 對 onlineUserMap 新增一筆上線紀錄
-    onlineUserMap[uid] = socket.id;
+router.socketConnection = function(){
 
-    const sql = `SELECT hasNewMessage from user where uid=${uid}`
-    db.query(sql, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            // 若該用戶有新訊息(去user檢查hasNewMessage欄位檢查是否為 1)
-            if (result[0].hasNewMessage == 1) {
-                //去 unread資料表將自己全部訊息select出來
-                getUnreadMessages(uid, socket.id);
-            };
-        };
-    });
-
-    // 接收 sendMessage 事件
-    socket.on('sendMessage', auth.auth, (roomId, content) => {
+    io.on('connection', auth.auth, (socket) => {
         var uid = auth.uid;
-        // 去 room 資料表找出一樣有 roomId 的資料
-        sql = `SELECT * from room where roomId=${roomId}`
+        // 對 onlineUserMap 新增一筆上線紀錄
+        onlineUserMap[uid] = socket.id;
+    
+        const sql = `SELECT hasNewMessage from user where uid=${uid}`
         db.query(sql, (err, result) => {
             if (err) {
                 console.log(err);
             } else {
-                // other用來記錄對方uID
-                var other;
-                if (uid == result[0].user1Id) {
-                    other = result[0].user2Id;
-                } else {
-                    other = result[0].user1Id;
+                // 若該用戶有新訊息(去user檢查hasNewMessage欄位檢查是否為 1)
+                if (result[0].hasNewMessage == 1) {
+                    //去 unread資料表將自己全部訊息select出來
+                    getUnreadMessages(uid, socket.id);
                 };
-
-                setMessages(uid, roomId, content);
-                // 對方在線 => 直接將收到的訊息放在 newMessage 事件中 發送
-                if (onlineUserMap[other] != null) {
-                    io.to(onlineUserMap[other]).emit('newMessage', content, function () {
-                        // set message status = 1 and set hasNewMessage = 0
-                        sql = `UPDATE user SET hasNewMessage=0 where uid=${uid}`
-                        db.query(sql, (err, result) => {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                sql = `UPDATE message SET status=1 where uid=${uid} and roomId=${roomId}`
-                                db.query(sql, (err, result) => {
-                                    if (err) {
-                                        console.log(err);
-                                    } else {
-                                        // console.log(result);
-                                    };
-                                });
-                            };
-                        });
-                    });
-                }
             };
         });
+    
+        // 接收 sendMessage 事件
+        socket.on('sendMessage', auth.auth, (roomId, content) => {
+            var uid = auth.uid;
+            // 去 room 資料表找出一樣有 roomId 的資料
+            sql = `SELECT * from room where roomId=${roomId}`
+            db.query(sql, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    // other用來記錄對方uID
+                    var other;
+                    if (uid == result[0].user1Id) {
+                        other = result[0].user2Id;
+                    } else {
+                        other = result[0].user1Id;
+                    };
+    
+                    setMessages(uid, roomId, content);
+                    // 對方在線 => 直接將收到的訊息放在 newMessage 事件中 發送
+                    if (onlineUserMap[other] != null) {
+                        io.to(onlineUserMap[other]).emit('newMessage', content, function () {
+                            // set message status = 1 and set hasNewMessage = 0
+                            sql = `UPDATE user SET hasNewMessage=0 where uid=${uid}`
+                            db.query(sql, (err, result) => {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    sql = `UPDATE message SET status=1 where uid=${uid} and roomId=${roomId}`
+                                    db.query(sql, (err, result) => {
+                                        if (err) {
+                                            console.log(err);
+                                        } else {
+                                            // console.log(result);
+                                        };
+                                    });
+                                };
+                            });
+                        });
+                    }
+                };
+            });
+        });
+        socket.on('ack')
     });
-    socket.on('ack')
-});
+}
 
 //去 unread資料表將自己全部訊息select出來
 function getUnreadMessages(uid, socketId) {
@@ -104,5 +108,5 @@ function setMessages(uid, roomId, content) {
         };
     });
 }
-
+module.exports = router;
 // location event
